a18f33cd890f5e8b558c67ce4f6f1aad96c77e92
bono-94 <bono.94@hotmail.com>
2023-03-19 13:11:27 +0000

    Fix migrations file error

--- projects/migrations/0004_auto_20230318_1811.py
+++ projects/migrations/0004_auto_20230318_1811.py
@@ -27,7 +27,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='note',
             name='username',
-            field=models.ForeignKey(default=django.utils.timezone.now, on_delete=django.db.models.deletion.CASCADE, related_name='user_note', to='auth.user'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='user_note', to='auth.user'),
             preserve_default=False,
         ),
         migrations.AddField(

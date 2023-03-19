87f81c0973720f15cb5ac48302cb43d313b66a7b
bono-94 <bono.94@hotmail.com>
2023-03-19 20:03:46 +0000

    Adjust order of urls paths

--- projects/urls.py
+++ projects/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('create-profile', views.UserProfileCreate.as_view(), name='create_profile'),
+    path('my-profile', views.get_user_profile, name='view_profile'),
     path('projects/', views.PostList.as_view(), name='all_projects'),
     path('projects/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('vote/<slug:slug>', views.PostVote.as_view(), name='vote'),
-    path('post-create', views.create_post, name='post_create'),
+    path('create-project', views.create_post, name='post_create'),
     path('my-projects', views.get_user_posts, name='user_posts'),
-    path('create-profile', views.UserProfileCreate.as_view(), name='create_profile'),
-    path('my-profile', views.get_user_profile, name='view_profile'),
-    path('contact', views.contact_page, name='contact')
+    path('contact', views.contact_page, name='contact'),
 ]

22a346954ab348493b63d727ca094d61093702cd
bono-94 <bono.94@hotmail.com>
2023-03-19 18:47:55 +0000

    Adjust links names on the base.html

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('projects/', views.PostList.as_view(), name='all_projects'),
+    path('projects/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('vote/<slug:slug>', views.PostVote.as_view(), name='vote'),
     path('post-create', views.create_post, name='post_create'),
     path('posts/<username>', views.UserPosts.as_view(), name='user_posts'),

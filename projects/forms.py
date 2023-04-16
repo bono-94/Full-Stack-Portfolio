2c019ff8335278ad50af0758c14b125d5e1142e4
bono-94 <bono.94@hotmail.com>
2023-04-16 20:35:26 +0000

    Update which fields to see from Profile model in a form

--- projects/forms.py
+++ projects/forms.py
@@ -6,19 +6,56 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = [
+            # PROFILE CARD
+            'privacy',
             'profile_color',
             'profile_image',
+            'profile_quote',
+            'active_days',
+            'active_hours',
+            'website_link',
+            'facebook_link',
+            'twitter_link',
+            'instagram_link',
+            'linkedin_link',
+            'youtube_link',
+            # GENERAL INFORMATION
+            'public_email',
             'first_name',
             'last_name',
             'location',
-            'company',
-            'occupation',
-            'public_email',
-            'website_link',
+            'languages',
+            'gender',
             'date_of_birth',
-            'work_history',
+            'height_cm',
+            'weight_kg',
+            # CURRENT EMPLOYMENT
+            'industry',
+            'company',
+            'department',
+            'occupation',
             'start_date',
+            'hours_per_week',
+            # PREVIOUS EMPLOYMENT
+            'cv',
+            'education',
+            'work_history',
+            'projects_portfolio',
+            'references',
+            'recommendations',
+            # ACHIEVEMENTS
             'projects_completed',
+            'goals_completed',
+            'missions_completed',
+            'milestones_completed',
+            'tasks_completed',
+            'contribution',
+            # ATTRIBUTES
+            'knowledge',
+            'skills',
+            'strengths',
+            'weaknesses',
+            # BUSINESS FOCUS
             'focus_innovation',
             'focus_financials',
             'focus_planning',
@@ -27,9 +64,42 @@ class ProfileForm(forms.ModelForm):
             'focus_quantity',
             'focus_collaboration',
             'focus_leadership',
+            # BUSINESS SPECIALTY
+            'special_ops',
+            'special_finance',
+            'special_marketing',
+            'special_supply_chain',
+            'special_hr',
+            'special_tech',
+            'special_sustainability',
+            'special_research',
+            # ACCOMPLISHMENTS
+            'results',       
+            'certificates',
+            'honors',
+            'articles',
+            'recognition',
+            'bigger_fish_results',
+            # REWARDS
+            'awards',
+            'business_rewards',
+            'innovation_land_rewards',
+            # PERSONAL WALL
+            'summary',
+            'story',
+            'journey',
+            'future',
+            'legacy',
+            'change',
+            'ideal_life',
+            'goals',
+            'motivation_wall',
+            'interests_hobbies_wall',
+            'daily_routine',
         ]
+        # PROFILE COLOR AS COLORPICKER
         widgets = {
-            'profile_color': forms.TextInput(attrs={'type': 'color'}), # specify the input type as 'color'
+            'profile_color': forms.TextInput(attrs={'type': 'color'}),
         }
 
 

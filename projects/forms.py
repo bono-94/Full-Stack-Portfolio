58af8a282582e43e1572f98e3983f3f336a115f2
bono-94 <bono.94@hotmail.com>
2023-04-25 22:41:52 +0000

    Addcustom validators for max value of 100 for profile progress bars

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Profile, Post, Note, Request
+from django.core.exceptions import ValidationError
 
 
 class ProfileForm(forms.ModelForm):
@@ -104,32 +105,28 @@ class ProfileForm(forms.ModelForm):
             'profile_color': forms.TextInput(attrs={'type': 'color'}),
         }
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     focus_fields = [
-    #         'focus_innovation',
-    #         'focus_financials',
-    #         'focus_planning',
-    #         'focus_monitoring',
-    #         'focus_quality',
-    #         'focus_quantity',
-    #         'focus_collaboration',
-    #         'focus_leadership',
-    #     ]
-    #     specialty_fields = [
-    #         'special_ops',
-    #         'special_finance',
-    #         'special_marketing',
-    #         'special_supply_chain',
-    #         'special_hr',
-    #         'special_tech',
-    #         'special_sustainability',
-    #         'special_research',
-    #     ]
-    #     for field in specialty_fields:
-    #         value = cleaned_data.get(field, 0)
-    #         if value < 0 or value > 100:
-    #             self.add_error(field, 'Value must be between 0 and 100.')
+    def clean(self):
+
+        cleaned_data = super().clean()
+        focus_fields = [
+            'focus_innovation',
+            'focus_financials',
+            'focus_planning',
+            'focus_monitoring',
+            'focus_quality',
+            'focus_quantity',
+            'focus_collaboration',
+            'focus_leadership',
+        ]
+        focus_values = [cleaned_data.get(field) for field in focus_fields]
+        for i in range(0, 8, 2):
+            if not all(focus_values[i:i+2]):
+                continue
+            if sum(focus_values[i:i+2]) != 100:
+                msg = f"The sum of {focus_fields[i].split('_', 1)[1]} and {focus_fields[i+1].split('_', 1)[1]} must be equal to 100."
+                self.add_error(focus_fields[i], msg)
+                self.add_error(focus_fields[i+1], msg)
+        return cleaned_data
 
 
 class PostForm(forms.ModelForm):

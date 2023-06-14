3524943074d9853e8d58850532a8508d4983f29e
bono-94 <bono.94@hotmail.com>
2023-06-14 10:25:41 +0000

    Make all published and draft posts render on different templates

--- projects/urls.py
+++ projects/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('create-project', views.PostCreate.as_view(), name='post_create'),
     path('projects/', views.PostList.as_view(), name='all_projects'),
     path('projects/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('my-project/<slug:slug>', views.PostDetailDraft.as_view(), name='post_detail_draft'),
     path('vote/<slug:slug>', views.PostVote.as_view(), name='vote'),
     path('my-projects', views.UserPosts.as_view(), name='user_posts'),
     path('post/edit/<int:post_id>/', views.post_edit, name='post_edit'),
